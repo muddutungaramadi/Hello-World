32e23db6b3a65a55673606484700bb7ffcdd58bc
muddutungaramadi <41056355+muddutungaramadi@users.noreply.github.com>
2018-12-04 09:17:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World--- Hey Adi this is your first Jenkins project");
+    System.out.println("\n Welcome to Ford Jenkins Server");
   }
 }
